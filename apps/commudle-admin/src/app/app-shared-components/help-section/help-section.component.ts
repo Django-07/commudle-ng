5f0826a76bc1d41e457554f44ba5895ce5fc03f0
Django-07 <bhavisya.aka.ratna@gmail.com>
2025-07-17 01:25:22 +0530

    fix(help-link): fixed the sidebar of help link

--- apps/commudle-admin/src/app/app-shared-components/help-section/help-section.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/help-section/help-section.component.ts
@@ -34,7 +34,10 @@ export class HelpSectionComponent implements OnInit {
   }
 
   openSidebar() {
+    console.log('openSidebar triggered');
+    console.log('helpDictionaryData:', this.helpDictionaryData);
     if (this.helpDictionaryData && this.helpDictionaryData.type === EHelpDictionaryType.URL) {
+      window.open(this.helpDictionaryData.url, '_blank');
       // this.helpDictionaryService.getHelpDictionaryIframe(this.helpDictionaryData.url);
       this.helpSidebarService.openSidebar('helpSection');
       this.gtmService(this.helpDictionaryData);
@@ -43,6 +46,7 @@ export class HelpSectionComponent implements OnInit {
 
   getHelpSectionData() {
     this.helpDictionaryData = this.helpDictionary[this.helpDictionaryName];
+    console.log('helpDictionaryData:', this.helpDictionaryData);
   }
 
   gtmService(helpData) {
